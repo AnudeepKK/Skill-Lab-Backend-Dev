63cb779448359386b47f25819f6c5c7216cf7e62
BhatNishanthGanesh <nishanth.cs21@sahyadri.edu.in>
2024-03-16 13:23:54 +0530

    done

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -6,5 +6,7 @@ const orderController = require('../controller/orderController');
 router.post('/placeorder', orderController.placeOrder);
 router.post('/feedback/:orderId',orderController.submitFeedback);
 router.post('/location', orderController.updateOrderLocation);
+router.get('/delivery/:orderId',orderController.deliverOrder)
+router.get('/verifyOTP/:orderId/:otp', orderController.verifyOtp);
 
 module.exports = router;
